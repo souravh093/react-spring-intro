85d6b99a334b5c8a9a4e7e0b721a89b1c1334052
Sourav halder <souravh093@gmail.com>
2023-07-20 11:57:36 +0600

    solving the interview question

--- interview.js
+++ interview.js
@@ -1,50 +1,11 @@
-// // Setup
-// const myArray = [];
-
-// // Only change code below this line
-// let i = 5;
-// while(i > -1) {
-//   myArray.push(i);
-//   i--;
-// }
-
-// console.log(myArray)
-
-
-// Setup
-// const myArray = [];
-
-// // Only change code below this line
-// for(let i = 1; i < 6; i++) {
-//   myArray.push(i)
-// }
-
-// console.log(myArray)
-
-// // Setup
-// const myArray = [];
-
-// // Only change code below this line
-// for(let i = 9; i > 0; i -= 2 ) {
-//   myArray.push(i)
-// }
-
-// console.log(myArray)
-
-
-function multiplyAll(arr) {
-    let product = 1;
-    // Only change code below this line
-    for(let i = 0; i < arr.length; i++) {
-      for(let k = 0; k < arr[i].lenght; k++) {
-        return arr[i][k]
-      }
-    }
-    // Only change code above this line
-    return product;
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
   }
-  
-  
-
-  console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]))
-  
+}
